3b8d3b380199695283081e1a0c24a2f2da8ac1c6
Lennart Pegel <github@justlep.net>
2023-09-28 11:39:27 +0200

    moved files

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import sharp from 'sharp';
 import {resolveProjectPath} from './lib/fsutil.js';
-import {basename} from 'node:path';
 
-const SWEEP_22K = resolveProjectPath('test-data/sweep-20-20000hz-48k-linear.png');
-const SWEEP_22K2 = resolveProjectPath('test-data/sweep-20-22000hz-48k-linear.png');
+const SWEEP_20K = resolveProjectPath('test-data/sweep-20-20000hz-48k-linear.png');
+const SWEEP_22K = resolveProjectPath('test-data/sweep-20-22000hz-48k-linear.png');
+const SWEEP_24K = resolveProjectPath('test-data/sweep-20-24000hz-48k-linear.png');
 
 const CHANNELS = 3;
 
-for (const inFilePath of [SWEEP_22K, SWEEP_22K2]) {
+for (const inFilePath of [SWEEP_20K, SWEEP_22K, SWEEP_24K]) {
 
     let image = sharp(inFilePath);
 
-    const outFilePath = resolveProjectPath(`output/${basename(inFilePath).replace('linear', 'mel')}`)
+    const outFilePath = inFilePath.replace(/-linear\.png$/, '-mel.png');
 
     const {width, height} = await image.metadata();
 
